8ddd0a709e3b3f88865031a274a5aa92d6473966
Kwaku Boamah <kwaku.boamah@petraonline.com>
2021-03-19 12:48:42 +0000

    move thousand test to new file 🏠

--- test/ToHumanTest/TestHundreds.cs
+++ test/ToHumanTest/TestHundreds.cs
@@ -13,27 +13,6 @@ namespace ToHumanTest
         }
 
         [Fact]
-        public void OneThousand()
-        {
-            var othou = 1000.ToHuman();
-            Assert.Equal("One Thousand", othou);
-        }
-
-        [Fact]
-        public void OneThousandOne()
-        {
-            var othouo = 1001.ToHuman();
-            Assert.Equal("One Thousand One", othouo);
-        }
-
-        [Fact]
-        public void OneThousandTwentyFive()
-        {
-            var othoutfive = 1025.ToHuman();
-            Assert.Equal("One Thousand Twenty Five", othoutfive);
-        }
-
-        [Fact]
         public void OneHundredTen()
         {
             var ohundten = 110.ToHuman();
---
+++ test/ToHumanTest/TestThousands.cs
@@ -0,0 +1,29 @@
+﻿using ToHuman;
+using Xunit;
+
+namespace ToHumanTest
+{
+    public class TestThousands
+    {
+        [Fact]
+        public void OneThousand()
+        {
+            var othou = 1000.ToHuman();
+            Assert.Equal("One Thousand", othou);
+        }
+
+        [Fact]
+        public void OneThousandOne()
+        {
+            var othouo = 1001.ToHuman();
+            Assert.Equal("One Thousand One", othouo);
+        }
+
+        [Fact]
+        public void OneThousandTwentyFive()
+        {
+            var othoutfive = 1025.ToHuman();
+            Assert.Equal("One Thousand Twenty Five", othoutfive);
+        }
+    }
+}
